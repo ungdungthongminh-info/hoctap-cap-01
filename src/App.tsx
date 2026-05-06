f25828a5c58987b1af6b2f6f9f08008e16d6a4db
ungdungthongminh-info <ungdungthongminh.info@gmail.com>
2026-05-06 10:33:45 +0700

    Mount static audio sync banner at app root

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { HashRouter } from 'react-router-dom';
 import { ThemeProvider } from './shared/themes';
 import { AppDataProvider } from './shared/providers/AppDataProvider';
 import { ToastProvider } from './shared/components/Toast';
+import { StaticAudioSyncBanner } from './shared/components/StaticAudioSyncBanner';
 import { isAdminUnlocked, unlockAdmin } from './shared/utils/adminAccess';
 import { STORAGE_KEYS } from './shared/constants/storageKeys';
 import { AppRoutes } from './routes/appRoutes';
@@ -182,6 +183,7 @@ export function App() {
         <AppDataProvider>
           <LicenseHeartbeat />
           <ToastProvider>
+            <StaticAudioSyncBanner />
             <HashRouter>
               <AppRoutes AdminGate={AdminGate} AdminErrorBoundary={AdminErrorBoundary} />
             </HashRouter>
